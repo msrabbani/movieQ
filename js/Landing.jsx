26a67b654ba2e36bd95117145a0ec8fee4d63b52
msrabbani <msrabbanii@gmail.com>
2018-09-18 06:15:40 +0700

    convert from fun component to es6 class component

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div className="app">
     <div className="landing">
       <h1>VideoQ</h1>
       <input type="text" placeholder="Search" />
-      <Link to='/search'> Browser All</Link>
+      <Link to="/search"> Browser All</Link>
     </div>
   </div>
 );
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import DataPilem from '../data.json'
-import ShowCard from './ShowCard'
+import React, { Component } from 'react';
+import DataPilem from '../data.json';
+import ShowCard from './ShowCard';
 
-const Search = () => (
-    <div className="search"> 
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: 'this is some sort debug statment'
+    };
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
         {/* <pre><code> {JSON.stringify(DataPilem, null,4)} </code></pre>*/}
 
         {/* {DataPilem.shows.map(show => <h3>{show.title}</h3> )}*/}
-        <div>
-        {DataPilem.shows.map(show => 
-            <ShowCard key={show.imdbID} {...show} />
-        )}
-        </div>
-    </div>
- )
 
-    export default Search
+        <header>
+          <h1>videoQ</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {DataPilem.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
