c8ea94457fc643ea86395e32a7924b45128fcf0c
msrabbani <msrabbanii@gmail.com>
2018-09-24 18:50:53 +0700

    Jest is testing framework

---
+++ js/__test__/Search.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Search from '../Search';
+
+test('Search renders correctly', ()=> {
+    const component =  renderer.create(<Search />)
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot()
+})
+
+
