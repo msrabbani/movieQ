83e96643dc14bc2bafbb5a2a89a1dea01dfb4f96
msrabbani <msrabbanii@gmail.com>
2018-10-01 20:29:37 +0700

    compares the filtered result to the expect show count

--- js/__test__/Search.test.jsx
+++ js/__test__/Search.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme'
 import Search from '../Search';
-import ShowCard  from '../ShowCard';
+import showCard  from '../ShowCard';
 import DataPilem from '../../data.json'
 
 
@@ -14,10 +14,15 @@ test('Search renders correctly', ()=> {
 
 test('Search should render correct amount of shows', () => {
     const component = shallow(<Search />)
-    expect(DataPilem.shows.length).toEqual(component.find(ShowCard).length)
+    expect(component.find(showCard).length).toEqual(DataPilem.shows.length)
 })
 
-xtest('Search should render correct amount of shows based on search term', () => {
+test('Search should render correct amount of shows based on search term', () => {
+    const searchWord = 'black';
+    const component = shallow(<Search />)
+    component.find('input').simulate('change', {target: {value: searchWord}})
 
+    const showCount = DataPilem.shows.filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
 
+    expect(component.find(showCard).length).toEqual(showCount)
 })
