476a8fb32ab37c69f33ba0155870e20ab1129aa9
msrabbani <msrabbanii@gmail.com>
2018-10-16 21:25:37 +0700

    header conditional display 1

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,14 +3,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const Header = () => (
+const Header = (props: { showSearch?: boolean }) => {
+    let utilSpace;
+
+    if(props.showSearch){
+        utilSpace = <h1>LOL</h1>
+    } else {
+        utilSpace = (
+            <h2>
+                <Link to="/search">Back</Link>
+            </h2>
+        )
+
+    }
+
+    return (
     <header>
         <h1>
             <Link to = '/'>
             videoQ
         </Link>
-        </h1>
+    </h1>
+    {utilSpace}
     </header>
-)
+    )
+}
+
+Header.defaultProps = {
+    showSearch: false
+}
 
 export default Header;
