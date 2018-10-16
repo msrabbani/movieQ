136580788ea4d102f82e4f01234feff9fa31e07d
msrabbani <msrabbanii@gmail.com>
2018-10-16 22:43:00 +0700

    header conditional display 2, passing props from(Search) parent to child(Header)

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,18 +3,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const Header = (props: { showSearch?: boolean }) => {
+const Header = (props: { showSearch?: boolean , handleSearchTermChange?: Function, searchTerm?: string }) => {
     let utilSpace;
 
     if(props.showSearch){
-        utilSpace = <h1>LOL</h1>
+        utilSpace = (
+          <input
+            onChange={props.handleSearchTermChange}
+            value={props.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+      )
     } else {
         utilSpace = (
             <h2>
                 <Link to="/search">Back</Link>
             </h2>
         )
-
     }
 
     return (
@@ -30,7 +36,9 @@ const Header = (props: { showSearch?: boolean }) => {
 }
 
 Header.defaultProps = {
-    showSearch: false
+    showSearch: false,
+    handleSearchTermChange: function noop() {},
+    searchTerm: ''
 }
 
 export default Header;
--- js/Search.jsx
+++ js/Search.jsx
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react';
 import ShowCard from './ShowCard';
+import Header from './Header';
 
 class Search extends Component {
   state = {
@@ -18,17 +19,8 @@ class Search extends Component {
   render() {
     return (
       <div className="search">
-        <header>
-            <h1>videoQ</h1>
-           <h1>{this.state.searchTerm.toUpperCase()}</h1>
-
-          <input
-            onChange={this.handleSearchTermChange}
-            value={this.state.searchTerm}
-            type="text"
-            placeholder="Search"
-          />
-        </header>
+          <Header searchTerm= {this.state.searchTerm} 
+              showSearch handleSearchTermChange= {this.handleSearchTermChange} />
         <div>
             {this.props.shows.filter(show =>
                 `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
