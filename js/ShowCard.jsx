1a9d18fe223707896ba291ec55f153bb0084b41c
msrabbani <msrabbanii@gmail.com>
2018-10-19 15:08:05 +0700

    React perf tools that automaticly hook into React intence and track wasted renders

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,7 +1,7 @@
 // @flow
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Wrapper = styled(Link)`
 width: 32%;
@@ -17,18 +17,33 @@ text-decoration: none;
 const Image = styled.img`
 width: 40%;
 float: left;
-margin-right: 10px`;
+margin-right: 10px;
+`;
 
-const ShowCard = (props: Show) => (
-    <Wrapper to= {`/details/${props.imdbID}`}>
-    <Image alt={`${props.title} Show Poster`} src={`public/img/posters/${props.poster}`} />
-    <div>
-      <h1>{props.title}</h1>
-      <h2>({props.year})</h2>
-      <p>{props.description}</p>
-    </div>
+class ShowCard extends Component {
 
-  </Wrapper>
-);
+    shouldComponentUpdate() {
+        return false;
+    }
+
+  props: Show;
+
+  render() {
+      return (
+      <Wrapper to={`/details/${this.props.imdbID}`}>
+        <Image
+          alt={`${this.props.title} Show Poster`}
+          src={`public/img/posters/${this.props.poster}`}
+        />
+        <div>
+          <h1>{this.props.title}</h1>
+          <h2>({this.props.year})</h2>
+          <p>{this.props.description}</p>
+        </div>
+
+      </Wrapper>
+    );
+  }
+}
 
 export default ShowCard;
