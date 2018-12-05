76f7efe14847a6168ed3590b812dd5beb519e4fc
msrabbani <msrabbanii@gmail.com>
2018-12-05 16:05:42 +0700

    Testing Thunk, handling asynchronous behavior and mock out AJAX request to test thunk

--- js/__test__/actionCreators.test.js
+++ js/__test__/actionCreators.test.js
@@ -1,21 +1,41 @@
 // @flow
+import moxios from 'moxios';
+import {setSearchTerm, addAPIData, getAPIDetails} from '../actionCreators';
 
-import {setSearchTerm, addAPIData} from '../actionCreators';
+const breakingBad = {
+  title: 'Breaking Bad',
+  year: '2008–2013',
+  description: "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
+  poster: 'bb.jpg',
+  imdbID: 'tt0903747',
+  trailer: 'XZ8daibM3AE',
+  rating: '7.9',
+};
 
 test('setSearchTerm', () => {
   expect(setSearchTerm('New York')).toMatchSnapshot();
 });
 
 test('addAPIData', () => {
-  expect(
-    addAPIData({
-      title: 'Breaking Bad',
-      year: '2008–2013',
-      description: "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
-      poster: 'bb.jpg',
-      imdbID: 'tt0903747',
-        trailer: 'XZ8daibM3AE',
-        rating: '7.9'
-    }),
-  ).toMatchSnapshot();
+  expect(addAPIData(breakingBad)).toMatchSnapshot();
 });
+
+test('getAPIDetails', (done: Function)=> {
+        const dispatchMock = jest.fn()
+    moxios.withMock(()=> {
+        getAPIDetails(breakingBad.imdbID)(dispatchMock)
+        moxios.wait(()=> {
+            const request = moxios.requests.mostRecent()
+            request
+                .respondWith({
+                    status: 200,
+                    response: breakingBad
+                })
+                .then(()=> {
+                    expect(request.url).toEqual(`http://localhost:3000/${breakingBad.imdbID}`)
+                    expect(dispatchMock).toBeCalledWith(addAPIData(breakingBad));
+                    done()
+                })
+        })
+    })
+})
