85b6da63ee13d1cf1beb2bb409e2c823c9d5ab8a
msrabbani <msrabbanii@gmail.com>
2018-12-12 22:04:35 +0700

    separate axios from action creator which aims to decrease size of bundle.js

---
+++ js/asyncActions.js
@@ -0,0 +1,16 @@
+// @flow
+
+import axios from 'axios'
+import {addAPIData} from './actionCreators'
+
+// making async request inside of thunk
+export function getAPIDetails(imdbID: string) { 
+    return (dispatch: Function) => {
+        axios.get(`http://localhost:3000/${imdbID}`)
+            .then(response => {
+                dispatch(addAPIData(response.data))
+
+            }). catch(error => {
+                console.error('axios error', error)}) // eslint-disable-line no-console
+    }
+}
