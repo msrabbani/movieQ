98bcd26b90b5b0c99e9e5e40c7b15cb30d880d52
msrabbani <msrabbanii@gmail.com>
2018-12-12 21:42:41 +0700

    building for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,57 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-    context: __dirname,
-    entry: [
-        'webpack-hot-middleware/client?path=__webpack_hmr&timeout=20000',"./js/ClientApp.jsx"],
-    devtool: "cheap-eval-source-map",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-        publicPath: '/public/'
-    },
-    devServer: {
-        hot: true,
-        publicPath: '/public/',
-        historyApiFallback: true
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json']
-    },
-    stats: {
-        colors: true,
-        reasons: true,
-        chunks: false
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin()
+const config = {
+  context: __dirname,
+  entry: [
+    'webpack-hot-middleware/client?path=__webpack_hmr&timeout=20000',
+    './js/ClientApp.jsx',
+  ],
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/',
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/public/',
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+      },
     ],
-    module: {
-        rules: [
-            {
-                enforce:'pre',
-                test: /\.jsx?$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader'
-            }
-        ]
-    }
+  },
+};
 
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'production') {
+  config.entry = './js/ClientApp.jsx'
+  config.devtool = false  
+  config.plugins = []
 }
+
+module.exports = config;
